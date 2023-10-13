69818486cc33e77b8de5fc2241540363c9a0f660
Milan Shah <mkshah5@ncsu.edu>
2023-10-13 10:15:11 -0400

    Added EMDenoise benchmark

---
+++ emdenoise/config.py
@@ -0,0 +1,8 @@
+from utils.utils import TrainingParams
+
+PARAMS = TrainingParams(batch_size=32, 
+                        chop_factor=4, 
+                        rpixels=256, 
+                        cpixels=256, 
+                        nchannels=1,
+                        is_base=False)
---
+++ emdenoise/emdenoise_torch.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+from pprint import pprint
+from pathlib import Path
+#from libpressio import PressioCompressor
+import sys
+sys.path.append("../")
+
+import argparse
+import os
+from distutils.util import strtobool
+from typing import Tuple
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from torchvision.transforms import ToTensor
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+import matplotlib.pyplot as plt
+
+from torch import Tensor
+from typing import Type
+
+from utils.utils import TrainingParams
+from compressor.compress_entry import compress, decompress, get_lhs_rhs_decompress
+from config import PARAMS
+from model import EMDenoiseNet
+
+TRAIN_SIZE = PARAMS.batch_size
+TEST_SIZE = PARAMS.batch_size
+CF = PARAMS.cf
+
+RPIX = PARAMS.rpix
+CPIX = PARAMS.cpix
+BD = PARAMS.BD
+RBLKS = PARAMS.rblks
+CBLKS = PARAMS.cblks
+
+IS_BASELINE_NETWORK = PARAMS.is_base
+
+if IS_BASELINE_NETWORK:
+    MODEL_NAME = "torch_base_resnet34"
+else:
+    MODEL_NAME = "torch_matmul_resnet34_cf"+str(CF)
+
+COMPRESSOR_PATH = "/home/mkshah5/SZ/build/bin/sz"
+
+# Dependent on the number of channels
+def full_comp(x, err=2.7e-3):
+    fshape = str(TRAIN_SIZE)+" "+str(PARAMS.nchannels)+" "+str(RPIX)+" "+str(CPIX)
+    n_x = x.numpy().astype(np.float32)
+    n_x.tofile('tmpb.bin')
+    c_command = COMPRESSOR_PATH+" -z -f -M ABS -A "+str(err)+" -i tmpb.bin -4 "+fshape
+    d_command = COMPRESSOR_PATH+" -x -f -s tmpb.bin.sz -4 "+fshape+" -i tmpb.bin"
+    os.system(c_command)
+    cr = (os.stat('tmpb.bin').st_size)/os.stat('tmpb.bin.sz').st_size
+    print("CR: "+str(cr))
+    os.system(d_command)
+    decomp = np.fromfile('tmpb.bin.sz.out',dtype=np.float32)
+    decomp = np.reshape(decomp, (TRAIN_SIZE,PARAMS.nchannels,RPIX,CPIX))
+    return torch.from_numpy(decomp)
+
+
+class EMDenoiseCompress(nn.Module):
+    def __init__(self):
+        super(EMDenoiseCompress, self).__init__()
+
+        self.internal_model = EMDenoiseNet()
+
+    # assume bs > 1
+    def forward(self, x):
+
+        out = self.internal_model(x)
+        return out
+    
+class EMDenoiseBase(nn.Module):
+    def __init__(self):
+        super(EMDenoiseBase, self).__init__()
+        self.internal_model = EMDenoiseNet()
+
+    # assume bs > 1
+    def forward(self, x):
+        out = self.internal_model(x)
+        return out
+
+def add_common_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--num-classes', type=int, default=10, help='Number of output classes')
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--weight-decay', type=float, default=0.0001)
+    parser.add_argument('--device',type=int,default=0)
+
+def add_run_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--train-torch', action='store_true', help='train FP32 PyTorch version of application')
+    parser.add_argument('-n', '--num-iterations', type=int, default=100, help='Number of iterations to run the pef for')
+    parser.add_argument('-e', '--num-epochs', type=int, default=50)
+    parser.add_argument('--log-path', type=str, default='checkpoints')
+    parser.add_argument('--dump-interval', type=int, default=1)
+    parser.add_argument('--num-workers', type=int, default=0)
+    parser.add_argument('--measure-train-performance', action='store_true')
+    parser.add_argument('--acc-test', action='store_true', help='Option for accuracy guard test in RDU regression.')
+    parser.add_argument('--data-dir',
+                        type=str,
+                        default='mnist_data',
+                        help="The folder to download the MNIST dataset to.")
+
+
+def prepare_fulldata(args: argparse.Namespace) -> Tuple[torch.utils.data.DataLoader]:
+
+    dataset_train = datasets.CIFAR10(
+        root='data',
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+    # CIFAR10 validation dataset.
+    dataset_valid = datasets.CIFAR10(
+        root='data',
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    # Create data loaders.
+    train_loader = DataLoader(
+        dataset_train, 
+        batch_size=TRAIN_SIZE,
+        shuffle=True
+    )
+    valid_loader = DataLoader(
+        dataset_valid, 
+        batch_size=TEST_SIZE,
+        shuffle=False
+    )
+
+    return train_loader, valid_loader
+
+
+def train(args: argparse.Namespace, model: nn.Module, optimizer,device) -> None:
+    train_loader, test_loader = prepare_fulldata(args)
+    loss_function = nn.MSELoss()
+    # Train the model
+    model = model.cuda()
+    total_step = len(train_loader)
+    for epoch in range(args.num_epochs):
+        avg_loss = 0
+        for i, (images, labels) in enumerate(train_loader):
+            labels = labels.cuda()
+            if not IS_BASELINE_NETWORK:
+                images = full_comp(images)
+            
+            
+            images = torch.mul(images, 255)
+            images = images.cuda()
+            run_start = time.time()
+                    
+            outputs = model(images)
+            
+            loss = loss_function(outputs,labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            run_end = time.time()
+            avg_loss += loss.mean()
+            run_time = run_end-run_start
+            print("===Timing===")
+            print("Step Run Time (s): "+str(run_time))
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, args.num_epochs, i + 1, total_step,
+                                                                     avg_loss / (i + 1)))
+        test_acc = 0.0
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            total_loss = 0
+            for images, labels in test_loader:
+                images = torch.mul(images, 255)
+            
+                if not IS_BASELINE_NETWORK:
+                    images = full_comp(images)
+                images.to(device)
+                outputs = model(images)
+                loss = loss_function(outputs,labels)
+                
+                total_loss += loss.mean()
+
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum()
+
+            test_acc = 100.0 * correct / total
+            print('Test Accuracy: {:.2f}'.format(test_acc),
+                  ' Loss: {:.4f}'.format(total_loss.item() / (len(test_loader))))
+    
+    torch.save(model, MODEL_NAME+".pt")
+
+def main():
+    parser = argparse.ArgumentParser()
+    add_common_args(parser)
+    add_run_args(parser)
+    args = parser.parse_args()
+    command = "train"
+    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+
+    if command == "test":
+        model = torch.load(MODEL_NAME+".pt").to(device)
+    else:
+        if IS_BASELINE_NETWORK:
+            model = EMDenoiseBase().to(device)
+        else:
+            model = EMDenoiseCompress().to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), 
+                                lr=args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+    train(args, model, optimizer,device)
+
+
+if __name__ == '__main__':
+    main()
