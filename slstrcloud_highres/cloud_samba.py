44694c71b7b4edb872e7d65b5b36631fcb46f204
Milan Shah <mkshah5@ncsu.edu>
2023-11-08 15:07:36 +0000

    Updated high res cloud

--- slstrcloud_highres/cloud_samba.py
+++ slstrcloud_highres/cloud_samba.py
@@ -115,9 +115,9 @@ class CloudMaskBase(nn.Module):
 
 def add_common_args(parser: argparse.ArgumentParser):
     parser.add_argument('--num-classes', type=int, default=10, help='Number of output classes')
-    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--lr', type=float, default=0.00001)
     parser.add_argument('--momentum', type=float, default=0.9)
-    parser.add_argument('--weight-decay', type=float, default=0.0001)
+    parser.add_argument('--weight-decay', type=float, default=0.00001)
     parser.add_argument('--config_path', type=str, default='./config-ch4.txt')
 
 
