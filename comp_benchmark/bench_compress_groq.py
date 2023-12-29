0852c43fa7eabfe85dd8216710111cb81425d3c9
Milan Shah <mkshah5@ncsu.edu>
2023-12-29 14:44:41 -0500

    Added compression model benchmarks

---
+++ comp_benchmark/bench_compress_groq.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+from distutils.util import strtobool
+from typing import Tuple
+import time
+import sys
+sys.path.append("../")
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from torchvision.transforms import ToTensor
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+#import matplotlib.pyplot as plt
+
+from torch import Tensor
+from typing import Type
+
+from groqflow import groqit
+
+from utils.utils import TrainingParams
+from compressor.compress_entry_f32 import compress_on_device, decompress, get_lhs_rhs_compress
+
+
+PARAMS = None
+TRAIN_SIZE = None
+TEST_SIZE = None
+CF = None
+RPIX = None
+CPIX = None
+BD = None
+RBLKS = None
+CBLKS = None
+IS_BASELINE_NETWORK = None
+MODEL_NAME = None
+
+BENCHMARK_NAME = "compressstandalone"
+VERSION = "groq"
+
+# Dependent on the number of channels
+
+
+class CompressorModel(nn.Module):
+    def __init__(self):
+        super(CompressorModel, self).__init__()
+
+        self.decompress = decompress
+        lhs, rhs = get_lhs_rhs_compress(PARAMS)        
+        self.lhs = torch.as_tensor(lhs).to(torch.float32)
+        self.rhs = torch.as_tensor(rhs).to(torch.float32)
+
+    # assume bs > 1
+    def forward(self, x):
+        r = compress_on_device(torch.squeeze(x[:,0,:,:]), self.lhs,self.rhs)
+        g = compress_on_device(torch.squeeze(x[:,1,:,:]), self.lhs,self.rhs)
+        b = compress_on_device(torch.squeeze(x[:,2,:,:]), self.lhs,self.rhs)
+        out = torch.stack((r,g,b),1)
+
+        #out = self.internal_model(out)
+
+        return out
+
+def add_common_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--num-classes', type=int, default=10, help='Number of output classes')
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--weight-decay', type=float, default=0.0001)
+    parser.add_argument('--config_path', type=str, default='./config-ch4.txt')
+    parser.add_argument('--command',type=str,default="full_pass")
+
+def add_run_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--train-torch', action='store_true', help='train FP32 PyTorch version of application')
+    parser.add_argument('-n', '--num-iterations', type=int, default=100, help='Number of iterations to run the pef for')
+    parser.add_argument('-e', '--num-epochs', type=int, default=50)
+    parser.add_argument('--log-path', type=str, default='checkpoints')
+    parser.add_argument('--dump-interval', type=int, default=1)
+    parser.add_argument('--num-workers', type=int, default=0)
+    parser.add_argument('--measure-train-performance', action='store_true')
+    parser.add_argument('--acc-test', action='store_true', help='Option for accuracy guard test in RDU regression.')
+    parser.add_argument('--data-dir',
+                        type=str,
+                        default='mnist_data',
+                        help="The folder to download the MNIST dataset to.")
+
+
+def get_inputs_compress():
+    images = torch.randn(TRAIN_SIZE, PARAMS.nchannels, RPIX, CPIX)
+    # images = full_comp(images)
+  
+    return images
+
+
+
+
+def main():
+    global PARAMS, TRAIN_SIZE, TEST_SIZE, CF, RPIX, CPIX, BD, RBLKS, CBLKS, IS_BASELINE_NETWORK, MODEL_NAME
+
+    parser = argparse.ArgumentParser()
+    add_common_args(parser)
+    add_run_args(parser)
+    args = parser.parse_args()
+    PARAMS = TrainingParams(args.config_path)
+    TRAIN_SIZE = PARAMS.batch_size
+    TEST_SIZE = PARAMS.batch_size
+    CF = PARAMS.cf
+
+    RPIX = PARAMS.rpix
+    CPIX = PARAMS.cpix
+    BD = PARAMS.BD
+    RBLKS = PARAMS.rblks
+    CBLKS = PARAMS.cblks
+
+    IS_BASELINE_NETWORK = PARAMS.is_base
+
+    if IS_BASELINE_NETWORK:
+        MODEL_NAME = VERSION+"_base_"+BENCHMARK_NAME
+    else:
+        MODEL_NAME = VERSION+"_dct_"+BENCHMARK_NAME+"_cf"+str(CF)
+
+    
+    inputs = get_inputs_compress()
+
+    model = CompressorModel()
+
+    inputs = {"x": inputs}
+    
+    groq_model = groqit(model, inputs, build_name=MODEL_NAME)
+
+    for i in range(10):
+        s1 = time.time() 
+        out = groq_model(**inputs)
+        s2 = time.time()
+        print("Step: "+str(0)+", Time(s): "+str(s2-s1))
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ comp_benchmark/bench_compress_samba.py
@@ -0,0 +1,162 @@
+import argparse
+import os
+from distutils.util import strtobool
+from typing import Tuple
+import time
+import sys
+sys.path.append("../")
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from torchvision.transforms import ToTensor
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+#import matplotlib.pyplot as plt
+
+from torch import Tensor
+from typing import Type
+
+import sambaflow.samba.utils as utils
+from sambaflow import samba
+from sambaflow.samba.env import use_mock_samba_runtime
+from sambaflow.samba.utils.argparser import parse_app_args
+from sambaflow.samba.utils.benchmark_acc import AccuracyReport
+from sambaflow.samba.utils.common import common_app_driver
+from sambaflow.samba.utils.pef_utils import get_pefmeta
+
+from utils.utils import TrainingParams
+from compressor.compress_entry import compress_on_device, decompress, get_lhs_rhs_compress
+# from config import PARAMS
+
+
+MOCK_SAMBA_RUNTIME = use_mock_samba_runtime()
+PARAMS = None
+TRAIN_SIZE = None
+TEST_SIZE = None
+CF = None
+RPIX = None
+CPIX = None
+BD = None
+RBLKS = None
+CBLKS = None
+IS_BASELINE_NETWORK = None
+MODEL_NAME = None
+
+BENCHMARK_NAME = "compressstandalone"
+VERSION = "samba"
+
+# Dependent on the number of channels
+# def full_comp(x):
+#     r = compress(torch.squeeze(x[:,0,:,:]), PARAMS)
+#     g = compress(torch.squeeze(x[:,1,:,:]), PARAMS)
+#     b = compress(torch.squeeze(x[:,2,:,:]), PARAMS)
+#     out = torch.stack((r,g,b),1)
+
+#     return out
+
+class CompressorModel(nn.Module):
+    def __init__(self):
+        super(CompressorModel, self).__init__()
+
+        lhs, rhs = get_lhs_rhs_compress(PARAMS)
+        self.lhs = samba.from_torch_tensor(torch.from_numpy(lhs).to(torch.bfloat16),name='lhs')
+        self.rhs = samba.from_torch_tensor(torch.from_numpy(rhs).to(torch.bfloat16),name='rhs')
+        self.lin = nn.Linear(3*32*32, 1)        
+    # assume bs > 1
+    def forward(self, x):
+        r = compress_on_device(torch.squeeze(x[:,0,:,:]), self.lhs,self.rhs)
+        g = compress_on_device(torch.squeeze(x[:,1,:,:]), self.lhs,self.rhs)
+        b = compress_on_device(torch.squeeze(x[:,2,:,:]), self.lhs,self.rhs)
+        out = torch.stack((r,g,b),1)
+        # out = torch.reshape(out,(-1,3*32*32))
+        # return self.lin(out)
+        return torch.sum(out,dim=[2,3])
+
+def add_common_args(parser: argparse.ArgumentParser):
+    parser.add_argument('--num-classes', type=int, default=10, help='Number of output classes')
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--weight-decay', type=float, default=0.0001)
+    parser.add_argument('--config_path', type=str, default='./config-ch4.txt')
+    parser.add_argument('--compressor', type=str,default='dct')
+
+
+def add_run_args(parser: argparse.ArgumentParser):
+    parser.add_argument('-n', '--num-iterations', type=int, default=100, help='Number of iterations to run the pef for')
+
+
+
+def get_inputs_compress(args: argparse.Namespace):
+    images = torch.randn(TRAIN_SIZE, PARAMS.nchannels, RPIX, CPIX)
+    images = samba.from_torch_tensor(images, name='image', batch_dim=0)
+
+        
+    return (images)
+
+
+def main():
+    global PARAMS, TRAIN_SIZE, TEST_SIZE, CF, RPIX, CPIX, BD, RBLKS, CBLKS, IS_BASELINE_NETWORK, MODEL_NAME
+
+    args = parse_app_args(dev_mode=True,
+                          common_parser_fn=add_common_args,
+                          test_parser_fn=add_run_args,
+                          run_parser_fn=add_run_args)
+    PARAMS = TrainingParams(args.config_path)
+    TRAIN_SIZE = PARAMS.batch_size
+    TEST_SIZE = PARAMS.batch_size
+    CF = PARAMS.cf
+
+    RPIX = PARAMS.rpix
+    CPIX = PARAMS.cpix
+    BD = PARAMS.BD
+    RBLKS = PARAMS.rblks
+    CBLKS = PARAMS.cblks
+
+    
+    MODEL_NAME = VERSION+"_dct_"+BENCHMARK_NAME+"_cf"+str(CF)
+    
+    utils.set_seed(256)
+
+    inputs = get_inputs_compress(args)
+    
+    model = CompressorModel()
+    samba.from_torch_model_(model)
+    print(inputs)
+    print(args.mapping)
+
+    optimizer = samba.optim.SGD(model.parameters(), lr=args.lr) if not args.inference else None
+    if args.command == "compile":
+        samba.session.compile(model,
+                              inputs,
+                              optimizer,
+                              name=MODEL_NAME,
+                              app_dir=utils.get_file_dir(__file__),
+                              squeeze_bs_dim=False,
+                              config_dict=vars(args),
+                              pef_metadata=get_pefmeta(args, model))
+
+    elif args.command == "run":
+        #Run Lenet
+        utils.trace_graph(model, inputs, optimizer, pef=args.pef, mapping=args.mapping)
+        for i in range(10):
+            samba.session.start_runtime_profile()
+            s1 = time.time()
+            outputs = samba.session.run(input_tensors=[inputs],
+                                            output_tensors=model.output_tensors,
+                                            section_types=["FWD"])
+            o = samba.to_torch(outputs[0])
+            s2 = time.time()
+            samba.session.end_runtime_profile(MODEL_NAME+'.log')
+            print("Step: "+str(i)+", Time(s): "+str(s2-s1))
+            print(o)
+
+
+if __name__ == '__main__':
+    main()
